265ed9b5cda3df4de6646fad1634300bc1a7a982
lucia <luciasucunza@est.frba.utn.edu.ar>
2018-06-12 23:15:33 -0300

    Correción: no trabajo más con medias señales

--- ECGanchodebanda.py
+++ ECGanchodebanda.py
@@ -6,98 +6,90 @@ Created on Wed May 16 11:48:38 2018
 @author: slucia
 """
 
+
 import wfdb
 import matplotlib.pyplot as plt
 import numpy as np
 
 n = 40980                                           #cantidad de muestras
-
-
-#   APERTURA DE SEÑALES Y ANOTACIONES
-signal, fields = wfdb.io.rdsamp('/home/slucia/Documentos/Se/222',   sampto = n )
 t = np.arange( 0, n, 1 )                            #rango de muestras
 
-#   CALCULO DE FFT
+
+#   APERTURA DE SEÑAL y CALCULO DE LA FFT
+#%%
+signal, fields = wfdb.io.rdsamp('101', pb_dir='mitdb',   sampto = n)
+
 resf = fields.get( 'fs' )/n                         #Resolucion
-frq = np.arange( 0, 360 , resf)                     #Rango de frecuencias
-frq = frq[range( n//2 )]                            #Mitad de Rdf por ser simetrica la fft
+rangof = np.arange( 0, 360 , resf)                  #Rango de frecuencias
 
 Y = np.fft.fft( signal[:,0] )                       #fft 
-Ymedi = Y[range( n//2 )]                            #Media fft por ser simetrica
-Ymodu = abs( Ymedi )                                #Modulo de la media fft
-Ynorm = Ymodu / n                                   #Modulo de la media fft normalizada
+Ymodu = abs( Y )                                    #Modulo de la media fft
 
 
-#   RECORTE DEL ESPECTRO PARA CIERTO PORCENTAJE DE ENERGIA
-YR = np.zeros( n//2, complex )
+#   CALCULO DE LA FFT RECORTADA PARA CIERTO PORCENTAJE DE ENERGIA
+#%% 
+Ymitad = Ymodu[range(n//2)] 
+YR = np.zeros( n, complex )
 i = 0  
 areaY = 0
-porcentaje = 0.95
+porcentaje = 0.98
 
     #Calculo de la integral entre 0 y ts/2 de Ynorm²
 for i in range( n//2 ):
-    areaY = areaY + ( (Ynorm[i])**2 )*resf
+    areaY = areaY + ( (Ymitad[i])**2 )*resf
     
     #Obtencion del espectro (for hasta -1 para tomar el valor de i=0)
 areaVariable = areaY
 for i in  np.arange( n//2-1, -1, -1 ):
     if areaVariable  > ( porcentaje*areaY ):
-        areaVariable  = areaVariable  - ( (Ynorm[i])**2 )*resf
+        areaVariable  = areaVariable  - ( (Ymitad[i])**2 )*resf
         YR[i] = 0 
-        frecCorte = frq[i]
+
+        frecCorte = rangof[i]
     else:
         YR[i] = Y[i]
+        YR[n-1-i] = Y[n-1-i]
         
-        
-YmoduR = abs( YR )                                          #Modulo de la media fft
-YnormR = YmoduR / n                                         #Modulo de la media fft normalizada
- 
+    # Aca si copio Y{n} se me modifica porque el ultimo valor no es simetrtico!!              
+YmoduR = abs( YR )
+
+
+#   CALCULO DE ANTITRANSFORMADA DE LA FFT RECORTADA
+#%%
+signalYR = np.fft.ifft( YR )
+signalYR = signalYR.real
+
 
 
 #   GRAFICA DEL ESPECTRO ORIGINAL Y DEL RECORTADO
+#%% 
 plt.figure( 1 )
 plt.subplot( 211 )
-plt.plot( frq, Ynorm , 'g' )
-plt.title( 'Espectro Normalizado Original' )
+plt.plot( rangof, Ymodu , 'g' )
+plt.title( 'Espectro Original' )
 plt.grid(True)
-plt.xlim( -0.2, 70 )
-plt.ylim( 0, 0.05)
-
 
 plt.subplot ( 212 )
-plt.plot( frq, YnormR , 'g' )
-plt.title( 'Espectro Normalizado Recortado' )
+plt.plot( rangof, YmoduR , 'b' )
+plt.title( 'Espectro Recortado' )
 plt.grid( True )
-plt.xlim( -0.2, 70 )
-plt.ylim( 0, 0.05 )
 
 plt.show()  
-    
-    
-#   CALCULO DE LA SEÑAL RECORTADA
-antiYR = np.fft.ifft( YR )
-antiY = np.fft.ifft( Ymedi )
-tmedi = np.arange( 0, n//2, 1 )  
 
-
-#   GRAFICA DE LA SEÑAL ORIGINAL Y DE LA RECORTADA
+    
+#   GRAFICO DE LA SEÑAL Y DE LA SEÑAL FILTRADA
+#%% 
 plt.figure(3)
-plt.plot( tmedi, antiYR , 'g'  )
-plt.title( 'Espectro Antitransformado Recortado' )
+plt.subplot( 211 )
+plt.plot( t, signal[:,0], 'g'  )
+plt.title( 'Señal Original' )
 plt.grid( True )
 plt.ylim( -0.65, 1.05 )
-plt.xlim( -0.5, 800 )
+plt.xlim( -0.5, 1600 )
 
-plt.figure(4)
-plt.plot( tmedi, antiY , 'g' )
-plt.title( 'Espectro Antitransformado' )
+plt.subplot( 212 )
+plt.plot( t, signalYR , 'b' )
+plt.title( 'Señal Filtrada' )
 plt.grid( True )
 plt.ylim( -0.65, 1.05 )
-plt.xlim( -0.5, 800 )    
-
-plt.figure(5)
-plt.plot( t, signal[:,0], 'g' )
-plt.title( 'Espectro Original' )
-plt.grid( True )
-plt.ylim( -0.65, 1.05 )
-plt.xlim( -0.5, 1600 )    
+plt.xlim( -0.5, 1600 ) 
